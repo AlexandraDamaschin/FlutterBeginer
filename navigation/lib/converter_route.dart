8abfcd31bee60b3b1225ea639cb3ed4f6fe36a6a
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-28 21:24:26 +0100

    converter_route changed to StatefulWidget

--- navigation/lib/converter_route.dart
+++ navigation/lib/converter_route.dart
@@ -4,7 +4,7 @@ import 'package:meta/meta.dart';
 import 'package:navigation/unit.dart';
 
 //Converter screen where users can input amounts to convert.
-class ConverterRoute extends StatelessWidget {
+class ConverterRoute extends StatefulWidget {
   //[Category]'s name.
   final String name;
 
@@ -25,11 +25,16 @@ class ConverterRoute extends StatelessWidget {
         assert(units != null);
 
   @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
-    final unitWidgets = units.map((Unit unit) {
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
-        color: color,
+        color: widget.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
