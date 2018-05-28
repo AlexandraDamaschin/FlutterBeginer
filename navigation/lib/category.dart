88bda1d4524085f8fc60159c2c54a154d25c1204
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-28 19:50:06 +0100

    basic app

---
+++ navigation/lib/category.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import 'package:navigation/unit.dart';
+// We use an underscore to indicate that these variables are private.
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
+class Category extends StatelessWidget {
+  final String name;
+  final ColorSwatch color;
+  final IconData iconLocation;
+  final List<Unit> units;
+
+  //Creates a [Category].
+  const Category({
+    Key key,
+    @required this.name,
+    @required this.color,
+    @required this.iconLocation,
+    @required this.units,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(iconLocation != null),
+        assert(units != null),
+        super(key: key);
+
+  /// Navigates to the [ConverterRoute].
+  void _navigateToConverter(BuildContext context) {
+    // TODO: Using the Navigator, navigate to the [ConverterRoute]
+  }
+
+  // Builds a custom widget that shows [Category] information.
+  @override
+  // This `context` parameter describes the location of this widget in the
+  // widget tree. It can be used for obtaining Theme data from the nearest
+  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
+  // See https://docs.flutter.io/flutter/material/Theme-class.html
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        child: InkWell(
+          borderRadius: _borderRadius,
+          highlightColor: color,
+          splashColor: color,
+          // We can use either the () => function() or the () { function(); }
+          // syntax.
+          // TODO: Update this onTap property to call _navigateToConverter()
+          onTap: () {
+            print('I was tapped!');
+          },
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              // There are two ways to denote a list: `[]` and `List()`.
+              // Prefer to use the literal syntax, i.e. `[]`, instead of `List()`.
+              // You can add the type argument if you'd like, i.e. <Widget>[].
+              // See https://www.dartlang.org/guides/language/effective-dart/usage#do-use-collection-literals-when-possible
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(
+                    iconLocation,
+                    size: 60.0,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ navigation/lib/category_route.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+import 'package:navigation/unit.dart';
+import 'package:navigation/category.dart';
+
+//variables
+// We use an underscore to indicate that these variables are private.
+final _backgroundColor = Colors.green[100];
+
+class CategoryRoute extends StatelessWidget {
+  const CategoryRoute();
+
+  //names
+  static const _categoryNames = <String>[
+    'Length',
+    'Area',
+    'Volume',
+    'Mass',
+    'Time',
+    'Digital Storage',
+    'Energy',
+    'Currency',
+  ];
+
+  //colors
+  static const _baseColors = <Color>[
+    Colors.teal,
+    Colors.orange,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.purpleAccent,
+    Colors.red,
+  ];
+
+  //Makes the correct number of rows for the list view.
+  Widget _buildCategoryWidgets(List<Widget> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => categories[index],
+      itemCount: categories.length,
+    );
+  }
+
+// Returns a list of mock [Unit]s.
+  List<Unit> _retrieveUnitList(String categoryName) {
+    return List.generate(10, (int i) {
+      i += 1;
+      return Unit(
+        name: '$categoryName Unit $i',
+        conversion: i.toDouble(),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final categories = <Category>[];
+
+    //iterate through the list
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        //each item has name, color and icon
+        name: _categoryNames[i],
+        color: _baseColors[i],
+        iconLocation: Icons.cake,
+        units: _retrieveUnitList(_categoryNames[i]),
+      ));
+    }
+
+    final listView = Container(
+      color: _backgroundColor,
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: _buildCategoryWidgets(categories),
+    );
+
+    final appBar = AppBar(
+      elevation: 0.0,
+      title: Text(
+        'Unit Converter',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 30.0,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: _backgroundColor,
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: listView,
+    );
+  }
+}
