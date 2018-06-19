4251628cdea37dad24b71ab0862eb0ec41e57732
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-06-19 20:23:03 +0100

    changes

--- input/lib/category_tile.dart
+++ input/lib/category_tile.dart
@@ -7,19 +7,17 @@ import 'category.dart';
 import 'unit_converter.dart';
 
 // We use an underscore to indicate that these variables are private.
-// See https://www.dartlang.org/guides/language/effective-dart/design#libraries
 const _rowHeight = 100.0;
 final _borderRadius = BorderRadius.circular(_rowHeight / 2);
 
-/// A [CategoryTile] to display a [Category].
+//A [CategoryTile] to display a [Category].
 class CategoryTile extends StatelessWidget {
   final Category category;
   final ValueChanged<Category> onTap;
 
-  /// The [CategoryTile] shows the name and color of a [Category] for unit
-  /// conversions.
-  ///
-  /// Tapping on it brings you to the unit converter.
+  //The [CategoryTile] shows the name and color of a [Category] for unit
+  // conversions.
+  // Tapping on it brings you to the unit converter.
   const CategoryTile({
     Key key,
     @required this.category,
@@ -29,7 +27,7 @@ class CategoryTile extends StatelessWidget {
         assert(onTap != null),
         super(key: key);
 
-  /// Navigates to the [UnitConverter].
+  // Navigates to the [UnitConverter].
   void _navigateToConverter(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute<Null>(
       builder: (BuildContext context) {
@@ -55,14 +53,12 @@ class CategoryTile extends StatelessWidget {
     ));
   }
 
-  /// Builds a custom widget that shows [Category] information.
-  ///
-  /// This information includes the icon, name, and color for the [Category].
+  // Builds a custom widget that shows [Category] information.
+  // This information includes the icon, name, and color for the [Category].
   @override
   // This `context` parameter describes the location of this widget in the
   // widget tree. It can be used for obtaining Theme data from the nearest
   // Theme ancestor in the tree. Below, we obtain the display1 text theme.
-  // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
@@ -83,7 +79,6 @@ class CategoryTile extends StatelessWidget {
               // There are two ways to denote a list: `[]` and `List()`.
               // Prefer to use the literal syntax, i.e. `[]`, instead of `List()`.
               // You can add the type argument if you'd like, i.e. <Widget>[].
-              // See https://www.dartlang.org/guides/language/effective-dart/usage#do-use-collection-literals-when-possible
               children: [
                 Padding(
                   padding: EdgeInsets.all(16.0),
